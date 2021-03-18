8f217d656a956e420767ddb055fa173e79771856
Michel Fernandes <michelbong1@hotmail.com>
2021-03-18 17:52:14 -0300

    adicionando projeto

---
+++ usuarios/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {
+        "nome_pagina": "Início da dashboard",
+    }
+
+    return render(request, "index.html", context)
+
