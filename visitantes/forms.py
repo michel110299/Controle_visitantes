59181c365ea5a0c8ede8e04698c5cf7744a12535
Michel Fernandes <michelbong1@hotmail.com>
2021-03-19 17:14:56 -0300

    implementação de botões

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -4,4 +4,21 @@ from visitantes.models import Visitante
 class VisitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
-        fields = "__all__"
+        fields = ["nome_completo","cpf","data_nascimento","numero_casa","placa_veiculo"]
+        error_messages = {
+            "nome_completo" : {
+                "required": "O nome completo do visitante é obrigatório para o registro"
+            },
+            "cpf" : {
+                "required": "O cpf do visitante é obrigatório para o registro"
+            },
+            "data_nascimento" : {
+                "required": "A data de nascimento do visitante é obrigatória para o registro",
+                "invalid": "Por favor, informe um formato válido para a data de nascimento (DD/MM/AAAA) "
+            },
+            "numero_casa" : {
+                "required": "Porfavor, informe o numero da casa a ser visitada"
+            },
+
+
+        }
--- visitantes/models.py
+++ visitantes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Visitante(models.Model):
 
     nome_completo = models.CharField(
@@ -51,6 +52,27 @@ class Visitante(models.Model):
         verbose_name = "Porteiro responsavél pelo registro",
         on_delete = models.PROTECT, 
     )
+
+    def get_horario_saida(self):
+        if self.horario_saida:
+            return self.horario_saida
+        return "Horário de saida não registrado"
+
+    def get_horario_autorizacao(self):
+        if self.horario_autorizacao:
+            return self.horario_autorizacao
+        return "Visitante aguardando autorização"
+
+    def get_morador_responsavel(self):
+        if self.moderador_responsavel:
+            return self.moderador_responsavel
+        return "Visitante aguardando autorização"
+        
+    def get_placa_veiculo(self):
+        if self.placa_veiculo:
+            return self.placa_veiculo
+        return "Veículo não registrado"
+    
     class meta:
         verbose_name = "Visitante"
         verbose_name_plural = "Visitantes"
@@ -58,3 +80,4 @@ class Visitante(models.Model):
     
     def __str__(self):
         return self.nome_completo 
+
